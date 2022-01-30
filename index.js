19bed05e3bc5e1c7332433d4d1fd7152cf14617c
IsaqueSantosSilva <g1isaque.santos@gmail.com>
2022-01-29 22:04:11 -0300

    aa

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.listen(port, () => {
 });
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "file:///C:/Users/g1isa/Desktop/index.html");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
