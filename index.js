c9cfe7fe72725f500a4313e22c0f93dd60c2387b
IsaqueSantosSilva <g1isaque.santos@gmail.com>
2022-01-29 22:16:38 -0300

    aa

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const cors = require("cors");
 const app = express();
 const routes = require("./routes/routes");
 let port = process.env.PORT || 3000;
@@ -10,12 +9,3 @@ app.use(routes);
 app.listen(port, () => {
   console.log(` Server running at http://localhost:${port}/`);
 });
-
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-});
-
