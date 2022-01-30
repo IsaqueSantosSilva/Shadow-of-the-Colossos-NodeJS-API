4706b839f218843f65d9ed0022f4e0920258442a
IsaqueSantosSilva <g1isaque.santos@gmail.com>
2022-01-29 23:03:30 -0300

    aa

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ const routes = require("./routes/routes");
 const cors = require("cors")
 let port = process.env.PORT || 3000;
 
-app.use(cors({
-  origin: "*",
-}));
+
 
 app.use(express.json());
 app.use(routes);
@@ -14,3 +12,7 @@ app.use(routes);
 app.listen(port, () => {
   console.log(` Server running at http://localhost:${port}/`);
 });
+
+app.use(cors({
+  origin: "*",
+}));
