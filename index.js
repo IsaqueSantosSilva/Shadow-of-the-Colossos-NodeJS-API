81e9c00c7c348dc147c0e6c9b130ede6073590aa
IsaqueSantosSilva <g1isaque.santos@gmail.com>
2022-01-29 22:57:52 -0300

    aa

--- index.js
+++ index.js
@@ -7,11 +7,10 @@ let port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(routes);
 
-
 app.listen(port, () => {
   console.log(` Server running at http://localhost:${port}/`);
 });
 
 app.use(cors({
-  origin: "*",
+  origin: "http://127.0.0.1:5500/index.html",
 }));
