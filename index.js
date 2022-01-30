88f58491886fd40e27769ec6f52cd0483a3bd0ef
IsaqueSantosSilva <g1isaque.santos@gmail.com>
2022-01-29 22:12:37 -0300

    aa

--- index.js
+++ index.js
@@ -11,13 +11,11 @@ app.listen(port, () => {
   console.log(` Server running at http://localhost:${port}/`);
 });
 
-// cors.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-// });
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+});
 
-header("Content-Type: aplication/json");
-header("Access-Control-Allow-Origin: *");
